d82c5b6690a2af89cc13d48221752b251b26e517
Håkan Alexander <hakan.alexander@gmail.com>
2021-05-27 20:38:13 +0200

    Add JsonLiveSource

---
+++ src/Toggles/Sources/JsonLiveSource.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+
+namespace Toggles.Sources
+{
+    internal class JsonLiveSource : SourceBase, IObserver<JObject>
+    {
+        private readonly IObservable<JObject> _innerSource;
+        private IDisposable _subscription;
+        private JObject _currentState;
+
+        public JsonLiveSource(IObservable<JObject> innerSource)
+        {
+            _innerSource = innerSource;
+        }
+
+        public override void Start()
+        {
+            _subscription = _innerSource.Subscribe(this);
+        }
+
+        public override void Stop()
+        {
+            _subscription?.Dispose();
+        }
+
+        public void OnCompleted()
+        {
+            
+        }
+
+        public void OnError(Exception error)
+        {
+            
+        }
+
+        public void OnNext(JObject value)
+        {
+            _currentState?.Properties().Except(value.Properties())
+                .ToList()
+                .ForEach(x =>
+                {
+                    if(x.Value.Type != JTokenType.Boolean)
+                        return;
+
+                    NotifyObservers(x.Name, false);
+                });
+
+            if (value.ToString() != _currentState?.ToString())
+            {
+                value.Properties()
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        if (x.Value.Type != JTokenType.Boolean)
+                            return;
+
+                        NotifyObservers(x.Name, x.Value.Value<bool>());
+                    });
+            }
+
+            _currentState = value;
+        }
+    }
+}
---
+++ tests/Toggles.Tests/Sources/JsonLiveSource_Tests.cs
@@ -0,0 +1,88 @@
+﻿using Moq;
+using Newtonsoft.Json.Linq;
+using System;
+using Toggles.Sources;
+using Xunit;
+
+namespace Toggles.Tests.Sources
+{
+    public class JsonLiveSource_Tests
+    {
+        public class InnerSourceMock : IObservable<JObject>, IDisposable
+        {
+            private IObserver<JObject> _observer;
+
+            public IDisposable Subscribe(IObserver<JObject> observer)
+            {
+                _observer = observer;
+                return this;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public void Send(object obj)
+            {
+                _observer.OnNext(JObject.FromObject(obj));
+            }
+        }
+
+        [Fact]
+        public void Should_not_notify_when_json_is_not_changed()
+        {
+            var innerSource = new InnerSourceMock();
+            var source = new JsonLiveSource(innerSource);
+
+            var observerMock = new Mock<IObserver<NewToggleValue>>();
+
+            source.Subscribe(observerMock.Object);
+
+            source.Start();
+
+            innerSource.Send(new { one = true, two = false });
+            innerSource.Send(new { one = true, two = false });
+
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(JsonLiveSource), Key = "one", Active = true }));
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(JsonLiveSource), Key = "two", Active = false }));
+        }
+
+        [Fact]
+        public void Should_notify_false_when_property_is_not_longer_present()
+        {
+            var innerSource = new InnerSourceMock();
+            var source = new JsonLiveSource(innerSource);
+
+            var observerMock = new Mock<IObserver<NewToggleValue>>();
+
+            source.Subscribe(observerMock.Object);
+
+            source.Start();
+
+            innerSource.Send(new { one = true, two = false });
+            innerSource.Send(new { two = false });
+
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(JsonLiveSource), Key = "one", Active = true }));
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(JsonLiveSource), Key = "two", Active = false }));
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(JsonLiveSource), Key = "one", Active = false }));
+        }
+
+        [Fact]
+        public void Should_notify_bool_values_from_json()
+        {
+            var innerSource = new InnerSourceMock();
+            var source = new JsonLiveSource(innerSource);
+
+            var observerMock = new Mock<IObserver<NewToggleValue>>();
+
+            source.Subscribe(observerMock.Object);
+
+            source.Start();
+
+            innerSource.Send(new {one=true, two=false});
+
+            observerMock.Verify(x => x.OnNext(new NewToggleValue{SourceType = typeof(JsonLiveSource), Key = "one", Active = true}));
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(JsonLiveSource), Key = "two", Active = false }));
+        }
+    }
+}
