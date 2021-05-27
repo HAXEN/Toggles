6eba79733bdbb7d61d9dddf373e96fc85c84b35e
Håkan Alexander <hakan.alexander@gmail.com>
2021-05-27 17:52:28 +0200

    Add Bootstrap extensions

---
+++ src/Toggles/Sources/DictionarySourceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Toggles.Sources
+{
+    public static class DictionarySourceRegistration
+    {
+        public static SourcesRegister AddDictionarySource(this SourcesRegister register, int priority, IDictionary<string, bool> values)
+        {
+            register.AddSource<DictionarySource>(priority, provider => new DictionarySource(values));
+            return register;
+        }
+    }
+}
---
+++ src/Toggles/Sources/EnvironmentSourceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Toggles.Sources
+{
+    public static class EnvironmentSourceRegistration
+    {
+        public static SourcesRegister AddEnvironmentSource(this SourcesRegister register, int priority, EnvironmentVariableTarget target, string prefix = null)
+        {
+            register.AddSource<EnvironmentSource>(priority, provider => new EnvironmentSource(target, prefix));
+            return register;
+        }
+    }
+}
