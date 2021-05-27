2d920ec6de246113ef33ebc99fa2c38549451254
Håkan Alexander <hakan.alexander@gmail.com>
2021-05-27 17:47:18 +0200

    Add EnvironmentSource

---
+++ src/Toggles/Sources/EnvironmentSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Toggles.Sources
+{
+    internal class EnvironmentSource : DictionarySource
+    {
+        public EnvironmentSource(EnvironmentVariableTarget target, string prefix = null) : base(CreateDictionary(Environment.GetEnvironmentVariables(target), prefix)) { }
+
+        private static IDictionary<string, bool> CreateDictionary(IDictionary dictionary, string prefix)
+        {
+            var values = new Dictionary<string, bool>();
+            if (dictionary == null)
+                return values;
+
+            foreach (DictionaryEntry entry in dictionary)
+            {
+                var key = entry.Key as string;
+                var val = entry.Value as string;
+
+                if(string.IsNullOrEmpty(key))
+                    continue;
+
+                if(string.IsNullOrEmpty(val))
+                    continue;
+
+                var trimmedKey = key;
+                if (string.IsNullOrEmpty(prefix) == false)
+                {
+                    if(key.Contains(prefix) == false)
+                        continue;
+
+                    trimmedKey = key.Replace(prefix, string.Empty);
+                }
+
+                if(bool.TryParse(val, out var actual))
+                    values.Add(trimmedKey, actual);
+            }
+
+            return values;
+        }
+    }
+}
---
+++ tests/Toggles.Tests/Sources/EnvironmentSource_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Toggles.Sources;
+using Xunit;
+
+namespace Toggles.Tests.Sources
+{
+    public class EnvironmentSource_Tests
+    {
+        [Fact]
+        public async void Should_notify_based_on_environments()
+        {
+            Environment.SetEnvironmentVariable("application_features_one", "true");
+            Environment.SetEnvironmentVariable("application_features_two", "false");
+
+            var observerMock = new Mock<IObserver<NewToggleValue>>();
+            var tokenSource = new CancellationTokenSource();
+
+            observerMock.Setup(x => x.OnCompleted()).Callback(() => tokenSource.Cancel());
+
+            var source = new EnvironmentSource(EnvironmentVariableTarget.Process, "application_features_");
+
+            source.Subscribe(observerMock.Object);
+            source.Start();
+
+            await Task.Delay(200, tokenSource.Token).ContinueWith(t => t);
+
+            observerMock.Verify(x => x.OnNext(new NewToggleValue{SourceType = typeof(EnvironmentSource), Key = "one", Active = true}));
+            observerMock.Verify(x => x.OnNext(new NewToggleValue { SourceType = typeof(EnvironmentSource), Key = "two", Active = false }));
+        }
+    }
+}
