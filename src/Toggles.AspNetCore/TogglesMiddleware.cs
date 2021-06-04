2ac3a19d7af8d4dae7f9ef1d6d77e93ae06408ed
Håkan Alexander <hakan.alexander@gmail.com>
2021-06-04 16:14:07 +0200

    Add middleware to view current states

---
+++ src/Toggles.AspNetCore/TogglesMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toggles.AspNetCore
+{
+    public class TogglesMiddleware 
+    {
+        private readonly RequestDelegate _next;
+
+        public TogglesMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task InvokeAsync(HttpContext context, IToggle toggles)
+        {
+            var text = toggles
+                .CurrentStates
+                .OrderBy(x => x.Key)
+                .Aggregate("", (acc, pair) => acc += $"{pair.Key}:{pair.Value}\n", s => s.TrimEnd('\n'));
+
+            return context.Response.WriteAsync(text, Encoding.UTF8);
+        }
+    }
+}
---
+++ src/Toggles.AspNetCore/TogglesMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Toggles.AspNetCore
+{
+    public static class TogglesMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseToggles(this IApplicationBuilder app, string path = "/toggles")
+        {
+            return app.Map(path, builder => builder.UseMiddleware<TogglesMiddleware>());
+        }
+    }
+}
