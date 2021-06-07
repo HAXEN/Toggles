7a4a75fd13d6bffb4420f11f56cc5e2b925bc07d
Håkan Alexander <hakan.alexander@gmail.com>
2021-06-07 15:17:41 +0200

    Add execution extension

---
+++ src/Toggles/ToggleExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Toggles
+{
+    public static class ToggleExtensions
+    {
+        public static ToggleExecutor If(this IToggle toggle, string key)
+        {
+            return new ToggleExecutor(toggle.IsOn(key));
+        }
+    }
+
+    public class ToggleExecutor
+    {
+        private readonly bool _isOn;
+
+        internal ToggleExecutor(bool isOn)
+        {
+            _isOn = isOn;
+        }
+
+        public ToggleExecutor Then(Action action)
+        {
+            if (_isOn)
+                action?.Invoke();
+
+            return this;
+        }
+
+        public ToggleExecutor Else(Action action)
+        {
+            if(!_isOn)
+                action?.Invoke();
+
+            return this;
+        }
+    }
+}
